0666c809fbfc24862cacdccb724e622585badb7c
salesiopark <salesiopark@gmail.com>
2025-10-23 14:53:31 +0900

    add comments

--- src/quadEncoder.h
+++ src/quadEncoder.h
@@ -1,14 +1,27 @@
 /* 2025년10월18일 초안 작성
  * rp2040-encoder-library(by G.D.Bruno)를 사용한
  * 모터 위치(radian)/각속도(radian/sec) 측정
- * overflow에 safe한 방식으로 작성됨
+ * edge count값의 overflow에 safe하게 작성됨
  */
 #include <pio_encoder.h>
 
 class QuadEncoder {
 public:
-  // pinA는 A상의 핀번호(B상은 pinA+1로 자동 지정됨)
-  QuadEncoder(byte pinA, int encoderPPR, int gearRatio = 1
+  /*
+    (1) B상은 pinA+1로 자동 지정됨
+    (2) encoderPPR은 축이 1회전했을 때 A상의 펄스 수
+      모터의 엔코더에 표기된 PPL이 A상과 B상의 펄스수를 더한 값인 경우도 있기 때문에
+      이건 직접 모터축을 1회전시켜 펄스 수를 세어서 확인해야 함
+    (3) 기어비는 기본값이 1로 지정되어있으므로
+       엔코더만 단독으로 사용이라면 인수를 안 넘김
+
+       ex)  QuadEncoder qEnc1(14, 13);    //엔코더 단독 사용
+            QuadEncoder qEnc1(14, 13, 18); //모터에 붙은 엔코더 사용(기어비 1:18)
+  */
+  QuadEncoder(
+    byte pinA,        // A상의 핀번호
+    int encoderPPR,   // 축이 1회전했을 때 A상의 펄수 수
+    int gearRatio = 1 // 기어비
   ) : _pioEnc(pinA),
       _CPR(4*encoderPPR*gearRatio),
       _radPerCount(TWO_PI/_CPR)
@@ -29,9 +42,11 @@ public:
   }
 
 
-  // 0~2pi 값(normalized radian)을 반환
-  // count가 overflow해도 deltaCount(=count - _prvCount)는
-  // 정확하게 계산되므로 overflow에 safe하다
+  /*
+   0~2pi 값(normalized radian)을 반환
+   count가 overflow해도 deltaCount(=count - _prvCount)는
+   정확하게 계산되므로 overflow에 safe하다
+  */
   double get_radian_normalized() {
     _updateCountLL();
     // 0 ~ 2π 범위로 정규화
@@ -41,11 +56,13 @@ public:
   }
 
 
-  // 회전각속도(radian/sec)를 반환
+  /*
+    회전각속도(radian/sec)를 반환
+    millis()에 overflow가 발생(49일 후)해도
+    deltaMillis는 정확한 값이므로 overflow에 safe함
+  */
   double get_speed_radian() {
     unsigned long timeMs = millis();
-    // millis()에 overflow가 발생(49일 후)해도
-    // deltaMillis는 정확한 값
     unsigned long deltaMillis = timeMs - _prvMillis;
     // dt가 너무 짧으면 이전 속도 반환
     if (deltaMillis < 5) return _prvSpeed;
@@ -79,11 +96,15 @@ public:
 
 
 private:
-  //CPR: Count (Edges of A/B) Per Revolution
-  //AB상의 모든 edge를 카운트하여 해상도를 4배 높인다
-  //(PioEncoder 클래스의 x4모드)
-  //get_rad()와 get_speed()메서드에서 사용되는 변수들을 분라하여
-  //독립적으로 동작하게금 작성
+  /*
+    CPR: Count (Edges of A/B) Per Revolution
+    default로 AB상의 모든 edge를 카운트하여 해상도를 4배 높인다
+    (PioEncoder 클래스의 x4모드)
+    따라서 _CPR = 4*encoderPPR*gearRatio 로 계산되어 저장됨
+
+    get_rad()와 get_speed()메서드에서 사용되는 변수들을 분라하여
+    두 메서드가 count값을 독립적으로 관리하게금 작성
+  */
 
   //공통으로 사용되는 멤버들
   PioEncoder    _pioEnc;
